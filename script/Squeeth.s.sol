d94caf504a5e923b0a902b96fb3368b306a29684
Skelectric <58760447+Skelectric@users.noreply.github.com>
2022-12-20 20:05:56 -0500

    Successful deployment on Goerli

--- script/Squeeth.s.sol
+++ script/Squeeth.s.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.17;
 import "forge-std/Script.sol";
 import "forge-std/Test.sol";
 
-import "./util/CreatePayoffDefinition.sol";
-import "./util/ReuseOrDeployProduct.sol";
+import "./util/stdJsonDB.sol";
 
 import "../src/interfaces/IController.sol";
 import "../src/interfaces/IProduct.sol";
@@ -27,10 +26,17 @@ contract DeploySqueeth is Script {
     IController iController;
     Squeeth squeeth;
 
+    string constant deployments = "/script/deployments.json";
+
+    uint256 coordinatorID;
+    uint256 deployerPrivateKey;
+    
+    address deployer;
+
     function run() external {
-        uint256 coordinatorID = vm.envUint("COORDINATOR_ID");
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployer = vm.addr(deployerPrivateKey);
+        coordinatorID = vm.envUint("COORDINATOR_ID");
+        deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        deployer = vm.addr(deployerPrivateKey);
 
         vm.recordLogs();
 
@@ -44,14 +50,28 @@ contract DeploySqueeth is Script {
         }
 
         // squeeth contract payoff provider
-        squeeth = new Squeeth();
+        try stdJsonDB.getAddr("ContractPayoffProvider_Squeeth", deployments) returns (address deployedAddr) {
+            squeeth = Squeeth(deployedAddr);
+            console.log("Using payoff provider at %s", address(squeeth));
+        } catch {
+            
+            vm.startBroadcast(deployerPrivateKey);
+            squeeth = new Squeeth();
+            vm.stopBroadcast();
+
+            console.log("Deployed new payoff provider to %s", address(squeeth));
+
+            // uncomment only if skipping simulation
+            // stdJsonDB.set("ContractPayoffProvider_Squeeth", address(squeeth), deployments);
+
+        }
 
         // squeeth product info
         // n ether = n * 1e18
         IProduct.ProductInfo memory productInfo = IProduct.ProductInfo({
-            name: "milli-Squeeth",
-            symbol: "mSQTH",
-            payoffDefinition: CreatePayoffDefinition.create(address(squeeth), false),
+            name: "skelly-Squeeth2",
+            symbol: "skSQTH2",
+            payoffDefinition: createPayoffDefinition(address(squeeth), false),
             oracle: IOracleProvider(CHAINLINK_ETHUSD_GOERLI),
             maintenance: UFixed18.wrap(0.3 ether),
             fundingFee: UFixed18.wrap(0.1 ether),
@@ -67,14 +87,61 @@ contract DeploySqueeth is Script {
             })
         });
 
-        ReuseOrDeployProduct.reuseOrDeploy(coordinatorID, iController, productInfo);
+        reuseOrDeploy(iController, productInfo);
 
+        // Vm.Log[] memory logs = vm.getRecordedLogs();
 
-        // vm.startBroadcast(deployerPrivateKey);
+    }
 
-        Vm.Log[] memory logs = vm.getRecordedLogs();
+    function reuseOrDeploy(IController controller, IProduct.ProductInfo memory productInfo) internal {
 
+        string memory deploymentName = string.concat("Product_", productInfo.symbol);
 
+        try stdJsonDB.getAddr(deploymentName, deployments) returns (address productAddress) {
+            console.log("Reusing %s at %s", deploymentName, productAddress);
+        } catch {
+            console.log("Creating %s...", deploymentName);
+
+            vm.startBroadcast(deployerPrivateKey);
+            (bool success, bytes memory data) = address(controller).call(
+                abi.encodeCall(IController.createProduct, (coordinatorID, productInfo))
+            );
+            vm.stopBroadcast();
+
+            require(success, "createProduct() failed");
+
+            (address productAddr) = abi.decode(data, (address));
+            IProduct product = IProduct(productAddr);
+
+            console.log("Created at %s", address(product));
+
+            // uncomment only if skipping simulation
+            // stdJsonDB.set(deploymentName, address(product), deployments);
+        }
+    }
+
+    function createPayoffDefinition(address contractAddress, bool short) 
+        internal pure returns (PayoffDefinition memory definition) 
+    {
+
+        definition = PayoffDefinition({
+            payoffType: PayoffDefinitionLib.PayoffType.PASSTHROUGH,
+            payoffDirection: PayoffDefinitionLib.PayoffDirection.LONG,
+            data: bytes30(0)
+        });
+
+        if (short) {
+            definition.payoffDirection = PayoffDefinitionLib.PayoffDirection.SHORT;
+        }
+
+        if (contractAddress != address(0)) {
+            definition.payoffType = PayoffDefinitionLib.PayoffType.CONTRACT;
+            definition.data = toBytes30(contractAddress);
+        }
+    }
+
+    function toBytes30(address addr) internal pure returns (bytes30) {
+        return bytes30(bytes20(addr)) >> 80;
     }
 
 }
